eb371f9969f63450412c174792864cb779bdb930
gilangpd <gilangpd634@gmail.com>
2022-06-01 18:50:22 +0700

    Aplikasi_Bacaan_Sholat

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
-import 'package:bacaan_sholat/page/main_page.dart';
+import 'package:bacaan_shalat/page/main_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: true,
       home: MainPage(),
     );
   }
